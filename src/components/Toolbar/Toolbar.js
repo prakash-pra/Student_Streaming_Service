3b09d24eb5469f1bfa0fd7e9da22f4e490ce1a96
prakash-pra <prakash287902@gmail.com>
2020-09-07 13:18:08 +1200

    external css added in toolbar component

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import toolbarStyle from './Toolbar.css';
+import './Toolbar.css';
 const toolbar = (props) => (
-  <nav className={'navbar fixed-top'} style={{ backgroundColor: '#1F9ED9' }}>
-    <a className='navbar-brand' href='login'>
+  <nav className={'navbar fixed-top mainNavbar'}>
+    <Link className='navbar-brand' pathName='/dashboad'>
       <img
         src={require('../../assets/Images/weltec-logo.svg')}
         alt='WelTec logo'
         width='150'
       />
-    </a>
+    </Link>
     <span className='navbar-text text-white'>
       <Link style={{ textDecoration: 'none', color: '#FFF' }}>Log out</Link>
     </span>
