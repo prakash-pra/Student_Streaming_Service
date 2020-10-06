83ee9b24e2f95d1b11d76e2af76da870c618e691
Tomato-byte <tts.amara19@gmail.com>
2020-10-06 18:07:54 +1300

    edited background color for button

--- src/containers/LoginUser/LoginUser.js
+++ src/containers/LoginUser/LoginUser.js
@@ -16,11 +16,11 @@ class loginUser extends Component {
           }}
         >
           <div className='card-header' style={{ backgroundColor: '#008da5' }}>
-            <img
+            <center><img
               src={require('../../assets/Images/officialLogo.png')}
               alt='weltec'
               width='190'
-            />
+            /></center>
           </div>
           <form className='form-signin'>
             <br />
@@ -42,7 +42,8 @@ class loginUser extends Component {
               />
             </div>
             <button
-              className='text-white btn btn-primary btn-lg btn-block'
+              style={{backgroundColor: '#008da5'}}
+              className='text-white btn btn-lg btn-block'      
               type='submit'
             >
               Sign in
